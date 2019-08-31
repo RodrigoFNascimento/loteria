6ede5b63c5529c5bc11ef6cdbdcb8e0e4f17cefc
RodrigoFNascimento <rodrigofn0@hotmail.com>
2019-08-31 11:37:55 -0300

    Add minimum heap

--- rodrigonascimento_201600155174_loteria.java
+++ rodrigonascimento_201600155174_loteria.java
@@ -103,26 +103,75 @@ public class rodrigonascimento_201600155174_loteria {
     }
 
     /**
-     * Turns a binary tree into a heap.
+     * Turns a binary tree into a max heap.
      * 
      * @param array Tree to be heapfied.
      * @param index Target index.
+     * @return      Heapified array.
      */
-    private static Person[] heapify(Person[] array, int index) {
+    private static Person[] heapifyMax(Person[] array, int index) {
 
         int root = index;
         int left = getLeft(index);
         int right = getRight(index);
-        if (left < array.length && array[left].score > array[index].score)
+        if (left < array.length && array[left].score > array[root].score)
             root = left;
-        if (right < array.length && array[right].score > array[index].score)
+        if (right < array.length && array[right].score > array[root].score)
             root = right;
         if (root != index) {
             swap(array, root, index);
-            array = heapify(array, root);
+            array = heapifyMax(array, root);
         }
 
         return array;
+    }
+
+    /**
+     * Turns a binary tree into a max heap.
+     * 
+     * @param array Tree to be heapfied.
+     * @param index Target index.
+     * @return      Heapified array.
+     */
+    private static Person[] heapifyMin(Person[] array, int index) {
+
+        int root = index;
+        int left = getLeft(index);
+        int right = getRight(index);
+        if (left < array.length && array[left].score < array[root].score)
+            root = left;
+        if (right < array.length && array[right].score < array[root].score)
+            root = right;
+        if (root != index) {
+            swap(array, root, index);
+            array = heapifyMin(array, root);
+        }
+
+        return array;
+    }
+
+    /**
+     * Gets the  names of the gamblers with the highest score.
+     * 
+     * @param gamblers      List of all the gamblers.
+     * @param rootIndex     Root of the subtree to be searched.
+     * @param winnersNames  Names of all the winners.
+     */
+    private static void getWinners(Person[] gamblers, int rootIndex, StringBuilder winnersNames) {
+
+        int left = getLeft(rootIndex);
+        int right = getRight(rootIndex);
+
+        if (winnersNames.length() > 0)
+            winnersNames.append('\n' + gamblers[rootIndex].name);
+        else
+            winnersNames.append(gamblers[rootIndex].name);
+
+        // Recurs down the tree
+        if (left < gamblers.length && gamblers[rootIndex].score == gamblers[left].score)
+            getWinners(gamblers, left, winnersNames);
+        if (right < gamblers.length && gamblers[rootIndex].score == gamblers[right].score)
+            getWinners(gamblers, right, winnersNames);
     }
 
     /**
@@ -171,8 +220,21 @@ public class rodrigonascimento_201600155174_loteria {
 
             int lastRootIndex = getLastRoot(gamblers.length);
 
+            // Turns the array into a max heap
             for (int i = lastRootIndex; i >= 0; i--)
-                gamblers = heapify(gamblers, i);
+                gamblers = heapifyMax(gamblers, i);
+
+            // Gets the names of the winners with the greatest score
+            StringBuilder topWinnersNames = new StringBuilder();
+            getWinners(gamblers, 0, topWinnersNames);
+
+            // Turns the array into a min heap
+            for (int i = lastRootIndex; i >= 0; i--)
+                gamblers = heapifyMin(gamblers, i);
+
+            // Gets the names of the winners with the smallest score
+            StringBuilder bottomWinnersNames = new StringBuilder();
+            getWinners(gamblers, 0, bottomWinnersNames);
     
         } catch (Exception ex) {
             ex.printStackTrace();
